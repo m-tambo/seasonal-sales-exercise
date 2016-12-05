c55f7a18c9bc0907576dc9e5bafa1325ff8183b9
Michael Tambornino <michael.tambornino@gmail.com>
2016-12-05 15:39:46 -0600

    added if statments to change product catgeories

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,39 +1,44 @@
-function showTheCategories(e) {
-   var categoriesData = JSON.parse(e.target.responseText);
+var categoriesData;
+var productsData;
+
+function loadCategories(e) {
+   categoriesData = JSON.parse(e.target.responseText);
    console.log("category data:", categoriesData);
-   // document.getElementById("categories").innerHTML = categoriesData;
 }
 
-function showTheProducts(k) {
-   var productsData = JSON.parse(k.target.responseText);
+
+
+function loadProducts(k) {
+   productsData = JSON.parse(k.target.responseText);
    console.log("product data:", productsData);
    var currentProduct;
    var currentCategory;
    var currentPrice;
+   var category;
+
    for (var i = 0; i < productsData.products.length; i++) {
-      // currentProduct += productsData.products[i].name;
-      // currentCategory += productsData.products[i].category_id;
-      // currentPrice += productsData.products[i].price;
+      if (productsData.products[i].category_id === 1) {
+         category = "Apparel";
+      } else if (productsData.products[i].category_id === 2) {
+         category = "Furniture";
+      } else if (productsData.products[i].category_id === 3) {
+         category = "Household";
+      }
       document.getElementById("products").innerHTML += `
-         <h3>Product: ${productsData.products[i].name}</3>
-         <p>Department: ${productsData.products[i].category_id}<p>
-         <p>Price: ${productsData.products[i].price}</p>
+         <h3 id="prodSpecs">Product: ${productsData.products[i].name}</3>
+         <p id="deptSpecs">Department: ${category}<p>
+         <p id="priceSpecs">Price: ${productsData.products[i].price}</p>
          <hr>
          `
    }
-
-   // console.log(productsData.products[5].name);
 }
 
-
 var categoriesRequest = new XMLHttpRequest();
-categoriesRequest.addEventListener("load", showTheCategories);
+categoriesRequest.addEventListener("load", loadCategories);
 categoriesRequest.open("GET", "categories.json");
 categoriesRequest.send();
 
-
-
 var productsRequest = new XMLHttpRequest();
-productsRequest.addEventListener("load", showTheProducts);
+productsRequest.addEventListener("load", loadProducts);
 productsRequest.open("GET", "products.json");
 productsRequest.send();
