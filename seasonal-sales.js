954f6185196f3e4175c9148b55416e35df350f1f
Michael Tambornino <michael.tambornino@gmail.com>
2016-12-05 14:26:16 -0600

    for loop sending json data to html

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,5 +1,29 @@
+function showTheCategories(e) {
+   var categoriesData = JSON.parse(e.target.responseText);
+   console.log("category data:", categoriesData);
+   // document.getElementById("categories").innerHTML = categoriesData;
+}
 
+function showTheProducts(k) {
+   var productsData = JSON.parse(k.target.responseText);
+   console.log("product data:", productsData);
+   var currentProduct;
+   var currentCategory;
+   var currentPrice;
+   for (var i = 0; i < productsData.products.length; i++) {
+      // currentProduct += productsData.products[i].name;
+      // currentCategory += productsData.products[i].category_id;
+      // currentPrice += productsData.products[i].price;
+      document.getElementById("products").innerHTML += `
+         <h3>Product: ${productsData.products[i].name}</3>
+         <p>Department: ${productsData.products[i].category_id}<p>
+         <p>Price: ${productsData.products[i].price}</p>
+         <hr>
+         `
+   }
 
+   // console.log(productsData.products[5].name);
+}
 
 
 var categoriesRequest = new XMLHttpRequest();
@@ -7,20 +31,9 @@ categoriesRequest.addEventListener("load", showTheCategories);
 categoriesRequest.open("GET", "categories.json");
 categoriesRequest.send();
 
-function showTheCategories(e) {
-   var categoriesData = JSON.parse(e.target.responseText);
-   console.log(categoriesData);
-   document.getElementById("categories").innerHTML = categoriesData;
-}
 
 
 var productsRequest = new XMLHttpRequest();
 productsRequest.addEventListener("load", showTheProducts);
 productsRequest.open("GET", "products.json");
 productsRequest.send();
-
-function showTheProducts(k) {
-   var productsData = JSON.parse(k.target.responseText);
-   console.log(productsData);
-   document.getElementById("products").innerHTML = productsData;
-}
